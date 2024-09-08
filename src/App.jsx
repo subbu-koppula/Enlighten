a569bff76079e6f00f865b6c72cf462da1888420
saisu-sai <saikoppulajee@gmail.com>
2024-09-08 19:52:03 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
 import { useState } from "react";
+import Deadlines from "./components/Deadlines";
+import Todo from "./components/Todo";
+import Header from "./components/Header";
 import "./App.css";
-
-function Header(){
-  return(
-    <p>This is a header!</p>
-  )
-}
-
-function Greeting(props){
-  console.log(props.name);
-  return(
-    <p>Greetings, {props.userName}</p>
-  )
-}
-
 
 
 function App() {
   const [count, setCount] = useState(0);
-  let greeting = "hello, adeventurer";
-  let userName = "sai";
   return (
     <>
       <Header ></Header>
       <div>
         {/* <Timetable></Timetable> */}
         <div>
-          <Greeting name = {userName}></Greeting>
-          <p>{greeting}</p>
-          {/* <Todo></Todo>
-          <Deadlines></Deadlines> */}
+          <Todo></Todo>
+          <Deadlines></Deadlines>
         </div>
       </div>
     </>
---
+++ src/components/Deadlines.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Deadlines = () => {
+  return (
+    <div>Deadlines</div>
+  )
+}
+
+export default Deadlines
